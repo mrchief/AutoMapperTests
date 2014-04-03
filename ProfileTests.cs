c9424fdd5642c6ef0173f2c6763146f3b47ea6bc
mrchief <mrchief_2000@yahoo.com>
2014-04-03 11:19:36 -0400

    Failing RecognizePostFix test

---
+++ ProfileTests.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoMapperTests
+{
+	internal class A
+	{
+		public string PropFlag { get; set; }
+	}
+
+	internal class B
+	{
+		public string Prop { get; set; }
+	}
+
+	internal class PrefixTestProfile : Profile
+	{
+		protected override void Configure()
+		{
+			RecognizePostfixes("Flag");
+			Mapper.CreateMap<A, B>();
+		}
+	}
+
+	[TestClass]
+	public class ProfileTests
+	{
+
+		public ProfileTests()
+		{
+			Mapper.AddProfile<PrefixTestProfile>();
+		}
+
+		[TestMethod]
+		public void ShouldRecognizeSourcePrefix()
+		{
+			var testData = new A { PropFlag = "someFlag" };
+			var actual = Mapper.Map<B>(testData);
+			Assert.AreEqual("someFlag", actual.Prop);
+		}
+	}
+}
